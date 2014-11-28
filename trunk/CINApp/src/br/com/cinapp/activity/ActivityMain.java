1f4f8930e3efe7947c5789c1ed7628b4bf3e7329
ricardo.franca86@gmail.com <ricardo.franca86@gmail.com@e7e0137f-467d-fae4-379a-c482b9f9138a>
2014-11-28 17:36:27 +0000

    -Atualização e inclusão de activitys

--- trunk/CINApp/src/br/com/cinapp/activity/ActivityMain.java
+++ trunk/CINApp/src/br/com/cinapp/activity/ActivityMain.java
@@ -1,19 +1,19 @@
 package br.com.cinapp.activity;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.appcompat.R.anim;
-import android.text.AndroidCharacter;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v7.widget.PopupMenu.OnMenuItemClickListener;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import br.com.cinapp.R;
 
-public class ActivityMain extends Activity {
+public class ActivityMain extends Activity{
 	
 	public final static String EXTRA_MESSAGE = "br.com.cinapp.cinapp.MESSAGE";
 
@@ -36,6 +36,24 @@ public class ActivityMain extends Activity {
     	EditText editText = (EditText) findViewById(R.id.edit_message);
     	String message = editText.getText().toString();
     	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);	
+    	startActivity(intent);
+    }
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    	
+    	switch (item.getItemId()) {
+    		case R.id.manter_perfil:
+    			startActivity(new Intent(this, ManterPerfilActivity.class));
+    			return true;
+    		case R.id.manter_Alimento:
+    			startActivity(new Intent(this, ManterAlimentoActivity.class));
+    			return true;
+    		case R.id.gerar_grafico:
+    			startActivity(new Intent(this, GerarGraficoActivity.class));
+    			return true;
+    		default:
+    			return super.onMenuItemSelected(featureId, item);
+    	}
     }
 }
