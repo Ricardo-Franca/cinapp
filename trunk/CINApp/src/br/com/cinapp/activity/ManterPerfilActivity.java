7a1d5f40a3b040123431e3e78b751a70fb27b97c
ricardo.franca86@gmail.com <ricardo.franca86@gmail.com@e7e0137f-467d-fae4-379a-c482b9f9138a>
2014-11-28 17:38:13 +0000

    -Atualização e inclusão de activitys

---
+++ trunk/CINApp/src/br/com/cinapp/activity/ManterPerfilActivity.java
@@ -0,0 +1,17 @@
+package br.com.cinapp.activity;
+
+import br.com.cinapp.R;
+import br.com.cinapp.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ManterPerfilActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_manter_perfil);
+	}
+}
